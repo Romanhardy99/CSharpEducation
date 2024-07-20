882146a6bf285bb8b2dc7dce9f036f70f41a154b
Romanhardy99 <kulenergystreet@mail.ru>
2024-07-20 17:58:25 +0400

    update

--- Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+297364e0e88eb1e849c5da9e143f4f8f9462b5f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b06b195f05ca2c480c52f9f47a3add475433bf6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
