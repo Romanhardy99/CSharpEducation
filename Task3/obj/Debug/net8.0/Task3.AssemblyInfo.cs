485362dd5be23e81b0b2a9012cdc0626c5f25a97
Romanhardy99 <kulenergystreet@mail.ru>
2024-07-22 11:33:33 +0400

    Task7 Задание 6 (Вычисление длины катетов и гипотенузы)

--- Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
+++ Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6afb85553c009d344490231b80f06ce599fa35f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df92af40ad2758810990103a4d01842697e37f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Task7/obj/Debug/net8.0/Task7.AssemblyInfo.cs
+++ Task7/obj/Debug/net8.0/Task7.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Task6")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df92af40ad2758810990103a4d01842697e37f5")]
-[assembly: System.Reflection.AssemblyProductAttribute("Task6")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Task6")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task7")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
