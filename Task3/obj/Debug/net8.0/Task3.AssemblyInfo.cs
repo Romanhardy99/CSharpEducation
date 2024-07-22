bd97472f9dfd9ae38b806de01e196b38da7d3695
Romanhardy99 <kulenergystreet@mail.ru>
2024-07-22 10:42:34 +0400

    Task3 Добавил 2 заданее

--- Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
+++ Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+882146a6bf285bb8b2dc7dce9f036f70f41a154b")]
-[assembly: System.Reflection.AssemblyProductAttribute("Task2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
