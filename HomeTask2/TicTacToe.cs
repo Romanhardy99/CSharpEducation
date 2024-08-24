5529b6c29177e3ccdd03f77d7e0c5d2e3d3bef86
Roman Kulyomin <limerance16@mail.ru>
2024-08-24 09:41:35 +0400

    добавил документации и регион для HomeTask2

--- HomeTask2/TicTacToe.cs
+++ HomeTask2/TicTacToe.cs
@@ -2,7 +2,7 @@
 
 namespace HomeTask2
 {
-    public class Program
+    public class TicTacToe
     {
         static char[,] board = new char[3, 3]
         {
@@ -10,29 +10,36 @@ namespace HomeTask2
             { '4', '5', '6' },
             { '7', '8', '9' }
         };
+
         static char Player1 = 'X';
         static char Player2 = 'O';
         static char Player;
+
         static void Main(string[] args)
         {
             bool game = true;
-           Player = Player1;
+            Player = Player1;
 
             while (game)
             {
                 DrawBoard();
+
                 PlayerMove();
 
                 if (CheckWin())
                 {
                     DrawBoard();
+
                     Console.WriteLine($"Игрок {Player} победил");
+
                     game = false;
                 }
                 else if (BoardFull())
                 {
                     DrawBoard();
+
                     Console.WriteLine("НИЧЬЯ!");
+
                     game = false;
                 }
                 else
@@ -40,15 +47,15 @@ namespace HomeTask2
                     SwapPlayer();
                 }
             }
-
-
-
-
         }
-
+        #region Методы 
+        /// <summary>
+        /// Отрисовка доски.
+        /// </summary>
         static void DrawBoard()
         {
             Console.Clear();
+
             for (int i = 0; i < 3; i++)
             {
                 Console.WriteLine($"{board[i, 0]} | {board[i, 1]} | {board[i, 2]}");
@@ -56,7 +63,9 @@ namespace HomeTask2
                     Console.WriteLine("---------");
             }
         }
-
+        /// <summary>
+        /// Проверка хода игрока.
+        /// </summary>
         static void PlayerMove()
         {
             bool validmove = false;
@@ -64,64 +73,70 @@ namespace HomeTask2
             while (!validmove)
             {
                 Console.WriteLine($"{Player} введите номер ячейки");
+
                 string input = Console.ReadLine();
 
                 int cell;
+
                 if (int.TryParse(input, out cell) && cell >= 1 && cell <= 9)
                 {
                     int row = (cell - 1) / 3;
+
                     int col = (cell - 1) % 3;
 
-                    //проверить свободна ли ячейка
-                    if (board[row, col] != 'X' && board[row, col] != 'O')
+                    if (board[row, col] != 'X' && board[row, col] != 'O') //Проверяем свободна ли ячейка.
                     {
                         board[row, col] = Player;
+
                         validmove = true;
-
-
                     }
                     else
                     {
                         Console.WriteLine("Ячейка занята");
-
                     }
                 }
                 else
                 {
                     Console.WriteLine("Неккоректный ввод");
-
                 }
             }
-
         }
-
+        /// <summary>
+        /// Смена сторон игроков.
+        /// </summary>
         static void SwapPlayer()
         {
-            Player = (Player == Player)? Player2 : Player1;
+            Player = (Player == Player1) ? Player2 : Player1;
         }
-
+        /// <summary>
+        /// Проверка игроков на победу.
+        /// </summary>
+        /// <returns>Возвращает тру если соответствует условию, иначе false.</returns>
         static bool CheckWin()
         {
-            //проверка строк
             for (int i = 0; i < 3; i++)
             {
-                if (board[i, 0] == Player && board[i, 1] == Player && board[i, 2] == Player)
+                if (board[i, 0] == Player && board[i, 1] == Player && board[i, 2] == Player) //Проверка заполнения строк.
                     return true;
             }
-            //столбцов
+
             for (int j = 0; j < 3; j++)
             {
-                if (board[0, j] == Player && board[1, j] == Player && board[2, j] == Player)
+                if (board[0, j] == Player && board[1, j] == Player && board[2, j] == Player) //Проверка заполнения столбцов.
                     return true;
             }
-            //по диагонали
-            if (board[0, 0] == Player && board[1, 1] == Player && board[2, 2] == Player)
+
+            if (board[0, 0] == Player && board[1, 1] == Player && board[2, 2] == Player) //Проверка заполнения по диагонали.
                 return true;
-            if (board[0, 2] == Player && board[1, 1] == Player && board[2, 0] == Player)
+
+            if (board[0, 2] == Player && board[1, 1] == Player && board[2, 0] == Player) //Проверка заполнения по диагонали.
                 return true;
             return false;
         }
-        //проверяю есть ли свободное место на доске 
+        /// <summary>
+        /// Проверка свободного места на доске.
+        /// </summary>
+        /// <returns></returns>
         static bool BoardFull()
         {
             for (int i = 0; i < 3; i++)
@@ -132,7 +147,6 @@ namespace HomeTask2
             }
             return true;
         }
-
-
+        #endregion
     }
 }
