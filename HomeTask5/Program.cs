2a98767d8a39f266312abc4b5bbcbf2d308f09b9
Romanhardy99 <kulenergystreet@mail.ru>
2024-09-23 14:53:36 +0400

    Скелет для дальнейшего редактирования создан

---
+++ HomeTask5/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace HomeTask5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public void UserInterface()
+        {
+            Console.WriteLine("Меню");
+            Console.WriteLine("1. Добавить пользователя");
+            Console.WriteLine("2. Удалить пользователя");
+            Console.WriteLine("3. Показать всех пользователей");
+            Console.WriteLine("4. Выход");
+            Console.WriteLine();
+            Console.Write("Выберите действие: ");
+        }
+
+        public void AddUser()
+        {
+            try
+            {
+                return;
+            }
+            catch (Exception) { Console.WriteLine("Ошибка."); }
+        }
+        public void RemoveUser()
+        {
+            try
+            {
+                return;
+            }
+            catch (Exception) { Console.WriteLine("Ошибка."); }
+        }
+
+        public void GetUser()
+        {
+            try
+            {
+                return;
+            }
+            catch (Exception) { Console.WriteLine("Ошибка."); }
+        }
+    }
+}
---
+++ HomeTask5/UserManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask5
+{
+    internal class UserManager
+    {
+        List<User> users = new List<User>();
+
+        public void Add(User user)
+        {
+            if (user == null)
+            {
+                this.users.Add(user);
+            }
+            else
+            {
+                throw new ArgumentNullException("Пользователь с таким id существует");
+            }
+        }
+
+        public void RemoveUser(int id)
+        {
+            try
+            {
+                var user = users.FirstOrderDefault(users => users.Id == id);
+
+                if (user == null)
+                {
+                    throw new ArgumentNullException("Пользователь не найден");
+                }
+                users.Remove(user);
+                Console.WriteLine("Пользователь удален");
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка при удалении");
+            }
+            return;
+        }
+
+        public void GetUser(int id)
+        {
+            return;
+        }
+
+        public List<User> GetAllUser()
+        {
+            return this.users;
+        }
+    }
+}
